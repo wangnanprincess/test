de9cd1dd849c23994cac66a2d3808578260896ce
wangnan <wangnanprincess@163.com>
2019-06-25 13:40:48 +0800

    0625

--- 11111111.py
+++ 11111111.py
@@ -13,13 +13,13 @@ def get_name(url):
     soup = BeautifulSoup(res.text, 'html.parser')  # 生成可分析对象
     #print(soup)
     if soup.find_all("a",class_="btn btn2"):
-        for name in soup.find_all("a",class_="btn btn2")[:100]:  # 遍历所有的姓氏链接，此处只获取前15个姓氏
+        for name in soup.find_all("a",class_="btn btn2")[:50]:  # 遍历所有的姓氏链接，此处只获取前100个姓氏
             url1 = 'http:' + name.attrs['href']  #找到姓氏链接，再次返回此函数
             get_name(url1)
             res1 = requests.get(url1)
             soup1 = BeautifulSoup(res1.text, 'html.parser')  # 生成可分析对象
             if soup1.find_all('a', class_='btn btn-link'):
-                for name in soup1.find_all('a', class_='btn btn-link')[:100]:  # 找到不同姓氏的名字，此处只获取每个姓氏的前10个
+                for name in soup1.find_all('a', class_='btn btn-link')[:50]:  # 找到不同姓氏的名字，此处只获取每个姓氏的前10个
                     name_list.append(name.text)
                     # print(name.text)
                 # return name.text
